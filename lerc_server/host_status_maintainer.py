27d98dabbe1102aa9e7f191dfcba2e49e693c776
Sean McFeely <mcfeelynaes@gmail.com>
2018-10-26 13:05:24 -0400

    added script to keep host statuses updated- run with cron

---
+++ lerc_server/host_status_maintainer.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import os
+import time
+import logging
+import configparser
+import pymysql
+import pymysql.cursors
+
+from datetime import datetime, timedelta
+
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+LOG_DIR = 'logs/'
+ETC_DIR = 'etc/'
+
+config = configparser.ConfigParser()
+config.read(os.path.join(BASE_DIR, ETC_DIR, 'lerc_server.ini'))
+
+# globals and config items
+DB_server = config['lerc_server']['dbserver']
+DB_user = config['lerc_server']['dbuser']
+DB_userpass = config['lerc_server']['dbuserpass']
+offline_timeout = int(config['host_checker']['offline_timeout'])
+
+# Connect to Db
+db = pymysql.connect(DB_server, DB_user, DB_userpass, "lerc", 
+                                        cursorclass=pymysql.cursors.DictCursor)
+
+# configure some logging
+logging.basicConfig(format='[%(levelname)s] %(asctime)s - %(name)s - %(message)s',
+                    filename=os.path.join(BASE_DIR, LOG_DIR, 'host_status.log'))
+LOGGER = logging.getLogger('host_status_maintainer')
+LOGGER.setLevel(logging.INFO)
+logging.propagate = False
+
+
+def status_update():
+
+    try:
+        with db.cursor() as c:
+            # hostname - status - install_date - last_activity - sleep_cycle - company_id
+            LOGGER.debug("Getting clients..")
+            c.execute("SELECT * FROM clients")
+            for client in c.fetchall():
+                if client['status'] != "UNKNOWN" and client['status'] != "UNINSTALLED" and client['status'] != "OFFLINE":
+                    away_time = datetime.now() - client['last_activity']
+                    LOGGER.debug("{} hasn't fetched in '{}'".format(client['hostname'], away_time))
+                    if away_time > timedelta(days=offline_timeout):
+                        c.execute("UPDATE clients SET status='UNKNOWN' WHERE hostname='{}'".format(client['hostname']))
+                        LOGGER.warning("Set {} to UNKNOWN: It's '{}' passed the configured offline timeout".format(client['hostname'],
+                                                                                   str(away_time - timedelta(days=offline_timeout))))
+                    elif away_time > timedelta(seconds=client['sleep_cycle']):
+                        c.execute("UPDATE clients SET status='OFFLINE' WHERE hostname='{}'".format(client['hostname']))
+                        LOGGER.info("Set {} to OFFLINE: Exceeded it's next expected check-in by '{}'".format(client['hostname'],
+                                                                         str(away_time - timedelta(seconds=client['sleep_cycle']))))
+
+    finally:
+        db.commit()
+    return
+
+if __name__ == '__main__':
+
+    status_update()
+    time.sleep(30)
+    status_update()
+    db.close()
+
+
