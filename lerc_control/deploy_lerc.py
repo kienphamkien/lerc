0decd8473103e9aedcf2aba0633833e5d0c01044
Sean McFeely <mcfeelynaes@gmail.com>
2021-07-14 14:54:25 -0400

    CBC deploy lerc

--- lerc_control/deploy_lerc.py
+++ lerc_control/deploy_lerc.py
@@ -2,42 +2,33 @@
 #/data/home/carbonblack/env3/bin/python3
 
 import os
-import re
 import sys
 import time
 import argparse
-import datetime
-import json
-import pprint
+import logging
 import coloredlogs
 
 from dateutil import tz
-from configparser import ConfigParser
 
 from lerc_control import lerc_api
-import logging
-
-# configure logging #
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s')
-# set noise level
-logging.getLogger('requests').setLevel(logging.WARNING)
-logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
-logging.getLogger('lerc_api').setLevel(logging.INFO)
-logging.getLogger('cbapi').setLevel(logging.WARNING)
 
 logger = logging.getLogger("lerc_control."+__name__)
-coloredlogs.install(level='DEBUG', logger=logger)
 
+from cbapi import auth
+from cbapi.response import *
 
-try:
-    from cbapi import auth
-    from cbapi.response import *
-    from cbapi.errors import ApiError, ObjectNotFoundError, TimeoutError
-except:
-    pass
-    #logging.debug("cbapi is not installed")
-    sys.exit(1)
+## New Cb Stuff ##
+from cbapi.psc import Device
+from cbapi.psc.threathunter import CbThreatHunterAPI
+from cbapi.errors import ConnectionError, UnauthorizedError, ServerError, ClientError
+
+from cbinterface.cli import load_configured_environments
+from cbinterface.config import get_default_cbapi_product, get_default_cbapi_profile
+from cbinterface.helpers import input_with_timeout
+from cbinterface.commands import ExecuteCommand, PutFile, GetFile, DeleteFile
+from cbinterface.psc.sessions import CustomLiveResponseSessionManager
+
+from cbinterface.psc.device import time_since_checkin, find_device_by_hostname, is_device_online
 
 HOME_DIR = os.path.dirname(os.path.realpath(__file__)) 
 
@@ -48,6 +39,7 @@ def eastern_time(timestamp):
     return eastern_time.astimezone(eastern_timebase).strftime('%Y-%m-%d %H:%M:%S.%f%z')
 
 
+## Old Cb Response stuff ##
 def go_live(sensor):
     start_time = time.time()
     timeout = 604800 # seven days
@@ -237,34 +229,224 @@ def CbSensor_search(profile, hostname):
             #return False
         logger.error("{}".format(str(e)))
         return False
+## end Cb Response functions ##
+
+## new CBC deployment ##
+def deploy_lerc_with_cbc(device: Device, install_command: str, lerc_installer_path: str, interactive: bool=False) -> lerc_api.Client:
+    """Deploy LERC to a Carbon Black Cloud Device.
+    
+    Args:
+        device: A Carbon Black Cloud Device
+        install_command: the command that installs LERC.
+        lerc_installer_path: path to a LERC installer package (MSI).
+    Returns:
+        An instance of the installed lerc_api.Client, if successful.
+    """
+
+    assert isinstance(device, Device)
+    
+    hostname = device.name[device.name.rfind('\\')+1:] if '\\' in device.name else device.name
+
+    # HACK: save a copy of any the https_proxy var
+    https_proxy = None
+    if 'https_proxy' in os.environ:
+        https_proxy = os.environ['https_proxy']
+
+    # create lerc session
+    ls = lerc_api.lerc_session()
+    # check and see if the client's already installed
+    client = None
+    try:
+        # NOTE: will remove proxy var from env
+        client = ls.get_host(hostname)
+        #os.environ['https_proxy'] = https_proxy
+    except:
+        logger.warning("Can't reach the lerc control server")
+
+    previously_installed = proceed_with_force = None
+    if client:
+        if client.status != 'UNINSTALLED':
+            logger.warning(f"lerc server reports the client is already installed on a system with this hostname:\n{client}")
+            proceed_with_force = input_with_timeout("Proceed with fresh install? (y/n) [n] ", default='n')
+            proceed_with_force = True if proceed_with_force == 'y' else False
+            if not proceed_with_force:
+                return None
+        else:
+            previously_installed = True
+            logger.info("A client was previously uninstalled on this host: {}".format(client))
+
+    cb = device._cb
+
+    timeout = 1200  # default 20 minutes (same used by Cb)
+    if not is_device_online(device):
+        # Decision point: if the device is NOT online, give the analyst and option to wait
+        logger.warning(f"{device.id}:{device.name} is offline.")
+        wait = "y"
+        if interactive:
+            prompt = "Would you like to wait for the host to come online? (y/n) [y] "
+            wait = input_with_timeout(prompt, default="y")
+        wait = True if wait.lower() == "y" else False
+        if not wait:
+            return None
+        timeout = 7
+        if interactive:
+            prompt = "How many days do you want to wait? [Default is 7 days] "
+            timeout = input_with_timeout(prompt, default=7)
+            if isinstance(timeout, str):
+                timeout = int(timeout)
+        if timeout > 30:
+            logger.warning(f"{timeout} days is a long time. Restricting to max of 30 days.")
+            timeout = 30
+
+        # 86400 seconds in a day
+        timeout = timeout * 86400
+
+    logger.info(f"waiting for active session on device ...")
+    session_manager = CustomLiveResponseSessionManager(cb, custom_session_keepalive=True)
+    if not session_manager.wait_for_active_session(device, timeout=timeout):
+        logger.error(f"reached timeout waiting for active session.")
+        return False
+
+      
+    download = PutFile(lerc_installer_path, 'lercSetup.msi')
+    execute = ExecuteCommand(install_command, wait_for_output=False, wait_timeout=60, wait_for_completion=True)
+
+    logger.info(f"submitting commands to download and install lerc.") 
+    if previously_installed:
+        # delete any old msi package, just in-case
+        session_manager.submit_command(DeleteFile('lercSetup.msi'), device)
+    session_manager.submit_command(download, device)
+    session_manager.submit_command(execute, device)
+    session_manager.process_completed_commands() # wait
+
+    # wait and anticipate the client check-in
+    wait = 5 #seconds
+    attempts = 6
+    logger.info("~ Giving client up to {} seconds to check in with the lerc control server..".format(attempts*wait))
+
+    for i in range(attempts):
+        try:
+            client = ls.get_host(hostname)
+        except:
+            logger.warning("Can't reach the lerc control server")
+            break
+        if client:
+            if client.status != 'UNINSTALLED':
+                break
+        logger.info("~ giving the client {} more seconds".format(attempts*wait - wait*i))
+        time.sleep(wait)
+
+    if not client or client.status == 'UNINSTALLED':
+        logger.warning("failed to auto-confirm install with lerc server.")
+        if previously_installed:
+            logger.warning("client never checked in.")
+        logging.info("getting install log...")
+        upload_log = GetFile('lerc_install.log', f"{hostname}_lerc_install.log")
+        session_manager.submit_command(upload_log, device)
+        session_manager.process_completed_commands()
+        return False
+
+    logger.info("Client installed on {} at '{}' - status={} - last check-in='{}'".format(hostname,
+                                 client.install_date, client.status, client.last_activity))
+    return client
+
+
 
 def main(argv):
 
-    parser = argparse.ArgumentParser(description="Use existing tools to install LERC")
-    cbR_profiles=auth.FileCredentialStore("response").get_profiles()
-    parser.add_argument('company', choices=cbR_profiles, help='specify an environment you want to work with.')
-    parser.add_argument('hostname', help="the name of the host to deploy the client to")
-    parser.add_argument('-p', '--package', help="the msi lerc package to install")
-    args = parser.parse_args()
+    # configure logging #
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s')
+    coloredlogs.install(level='INFO', logger=logger)
 
-    print(time.ctime() + "... starting")
+    # load carbonblack environment profiles #
+    configured_environments = load_configured_environments()
+    environments = []
+    # create human friendly options for the CLI
+    for product, profiles in configured_environments.items():
+        for profile in profiles:
+            environments.append(f"{product}:{profile}")
 
-    # ignore the proxy
-    #del os.environ['https_proxy']
+    # chose the default environment
+    default_product_name = get_default_cbapi_product()
+    default_profile_name = get_default_cbapi_profile()
+    default_environments = [env for env in environments if env.startswith(default_product_name)]
+    default_environment = f"{default_product_name}:{default_profile_name}"
+    default_environment = (
+        default_environment if default_environments and default_environment in default_environments else environments[0]
+    )
 
     # get the config items we need
     required_keys = ['client_installer', 'lerc_install_cmd']
     config = lerc_api.load_config(required_keys=required_keys)
     default_lerc_path = config['default']['client_installer'] #'/opt/lerc/lercSetup.msi'
+    lerc_environments = config['default']['environments'].split(',') if 'environments' in config['default'] else ['default']
 
-    sensor = CbSensor_search(args.company, args.hostname)
+    parser = argparse.ArgumentParser(description="Use existing tools to install LERC")
+    parser.add_argument("-d", "--debug", action="store_true", help="Turn on debug logging.")
+    parser.add_argument('lerc_env', choices=lerc_environments, default='default', help='Specify the LERC environment the client should belong in.')
+    parser.add_argument('--env', choices=environments, default=default_environment, help=f'Specify the Carbon Black environment to work with. default={default_environment}')
+    parser.add_argument('hostname', help="the name of the host to deploy the client to")
+    parser.add_argument('-p', '--package', default=default_lerc_path, help="the msi lerc package to install")
+    args = parser.parse_args()
 
-    result = deploy_lerc(sensor, config[args.company]['lerc_install_cmd'], environment=args.company, lerc_installer_path=args.package)
+    print(time.ctime() + "... starting")
+
+    if args.debug:
+        logging.getLogger("urllib3.connectionpool").setLevel(logging.INFO)
+        coloredlogs.install(level="DEBUG", logger=logger)
+
+    # ignore the proxy
+    #del os.environ['https_proxy']
+
+    product, profile = args.env.split(":", 1)
+
+    install_command = config[args.lerc_env]['lerc_install_cmd']
+
+    if product == "response":
+        sensor = CbSensor_search(profile, args.hostname)
+
+        result = deploy_lerc(sensor, install_command, environment=profile, lerc_installer_path=args.package)
+        if result:
+            print()
+            print(result)
+            print()
+            return True
+        return
+
+    # new stuff - technically the old stuff works with the new stuff but agilitiy
+    logger.debug(f"using '{profile}' profile via the configured '{product}' product.")
+    cb = None
+    try:
+        if product == "response":
+            cb = CbResponseAPI(profile=profile)
+        elif product == "psc" or product == "cbc":
+            cb = CbThreatHunterAPI(profile=profile)
+    except ConnectionError as e:
+        logger.critical(f"Couldn't connect to {product} {profile}: {e}")
+    except UnauthorizedError as e:
+        logger.critical(f"{e}")
+    except ServerError as e:
+        logger.critical(f"CB ServerError 😒 (try again) : {e}")
+    except TimeoutError as e:
+        logger.critical(f"TimeoutError waiting for CB server 🙄 (try again) : {e}")
+
+    # try and get the device
+    logger.info(f"searching for device...")
+    device = None
+    try:  # if device.id
+        device = Device(cb, args.hostname)
+    except ClientError:
+        device = find_device_by_hostname(cb, args.hostname)
+
+    if not device:
+        logger.error("could not get device.")
+        return 
+
+    # assume cbc/psc
+    result = deploy_lerc_with_cbc(device, install_command, default_lerc_path, interactive=True)
     if result:
-        print()
         print(result)
-        print()
-
 
 
 if __name__ == "__main__":
